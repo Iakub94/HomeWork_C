72bd3ea4f68757c5a1d7ee2a129b7c3d6f20c6f1
Iakub94 <119245021+Iakub94@users.noreply.github.com>
2022-12-05 11:27:59 +0300

    First order is performed

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Домашняя работа по семинару 1. 
+
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine("Number1");
+}
+else
+{
+    Console.WriteLine("Number2");
+}
+*/
+
+// Домашняя работа по семинару 2.
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
